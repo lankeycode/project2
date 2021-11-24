b222c7aaeb5c57810aa2829ba204bffb248b0318
lankey <michael.moussaoui@gmail.com>
2021-11-24 15:24:11 +0100

    #3 modified htlm & css / #1 modified js

---
+++ main.js
@@ -0,0 +1,8 @@
+const sideLeft = document.getElementById('left');
+const sideRight = document.getElementById('right');
+
+let dice = document.getElementById('dice');
+let global1 = document.querySelector('.scoreGlobal1')
+let global2 = document.querySelector('.scoreGlobal2')
+let round1 = document.querySelector('.scoreRound1')
+let round2 = document.querySelector('.scoreRound2')
