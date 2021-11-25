6b5751fcf607a8c40d3b3ec54cd06c797a32b139
lankey <michael.moussaoui@gmail.com>
2021-11-25 22:26:33 +0100

    #4 modified js / create 3 functions

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ const sideLeft = document.getElementById('left');
 const sideRight = document.getElementById('right');
 
 var dice = document.getElementById('dice');
+var diceImg = document.getElementById('.diceImg ')
 var newGame = document.getElementById('newGame');
 var rollDice = document.getElementById('rollDice');
 var hold = document.getElementById('hold');
-var global1 = document.querySelector('.scoreGlobal1');
-var global2 = document.querySelector('.scoreGlobal2');
+var global1 = document.querySelector('.scoreGlobalOne');
+var global2 = document.querySelector('.scoreGlobalTwo');
 var round1 = document.querySelector('.scoreRound1');
 var round2 = document.querySelector('.scoreRound2');
 
-var player1 = 'active';
-var round = 0;
-var global = 0;
+var playerActive = 'pl1';
+var round = '';
+var global = '';
+
+
+
 
 function getRandomDice(min, max) {
     min = Math.ceil(min);
@@ -20,39 +24,71 @@ function getRandomDice(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function changePlayer() {
+
+    playerActive === 'pl1' ? playerActive = 'pl2' : playerActive = 'pl1';
+
+    round = 0;
+    round1.textContent = round;
+    round2.textContent = round;
+}
+
 
 
 rollDice.addEventListener('click', () => {
     var result = getRandomDice(1, 6);
-    console.log(result);
+    document.getElementById('diceImg').src = "./img/dice" + result + ".svg";
 
 
-    if (result === 1) {
-        dice.textContent = result;
-        round = 0;
-        round1.textContent = round;
-        changePlaye()
-
-    } else {
-        dice.textContent = result;
+    if ((result !== 1) && (playerActive === 'pl1')) {
         round += result;
         round1.textContent = round;
+
+
+
+
+    } else if ((result !== 1) && (playerActive === 'pl2')) {
+        round += result;
+        round2.textContent = round;
+
+
+    } else {
+
+        changePlayer();
+
+    }
+})
+
+
+hold.addEventListener('click', () => {
+
+    if (playerActive === 'pl1') {
+        global += round1;
+        global1.textContent = global;
+        changePlayer();
+
+    } else {
+        global += round2;
+        global2.textContent = global;
+        changePlayer();
 
     }
 
 
-
-
 })
 
-hold.addEventListener('click', () => {
+function init() {
+
+    global = 0;
+    round = 0;
+    dice.style.display = none;
 
 
-})
 
-
+}
 newGame.addEventListener('click', () => {
 
 
 
 })
+
